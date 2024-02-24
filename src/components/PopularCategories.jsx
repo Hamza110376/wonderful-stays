3341a133c6a9a3c6b121e2b6298d9aded031e566
Hamza110376 <Muhammadhamzaawan110@gmail.com>
2024-02-24 19:40:01 +0500

    updated

--- src/components/PopularCategories.jsx
+++ src/components/PopularCategories.jsx
@@ -1,56 +1,43 @@
-import Styles from "./PopularCategories.module.css"
-function PopularCategories({categories}) {
-
-
+import Styles from "./PopularCategories.module.css";
+function PopularCategories({ categories }) {
   return (
-
-   
     <>
-        <div className={Styles.main}>
-<p>Categories</p>
-<h1>Popular Categories</h1>
-<div className={Styles.wrapper}>
+      <div className={Styles.main}>
+        <p>Categories</p>
+        <h1>Popular Categories</h1>
+        <div className={Styles.wrapper}>
+          <div className={Styles.card}>
+            <img className={Styles.wellness} src="/src/assets/wellness.jpg" />
+            <div className={Styles.inner}>
+              <h3 className={Styles.H3}>Wellness & spa</h3>
+              <p className={Styles.P}>Accommodation</p>
+              <div className={Styles.left}>
+                <img
+                  className={Styles.image}
+                  src="/src/assets/arrow-right.png"
+                />
+              </div>
+            </div>
+          </div>
 
-<div className={Styles.card}>
-<img className={Styles.wellness} src="/src/assets/wellness.jpg"/>
-<div className={Styles.inner}>
-<h3 className={Styles.H3}>Wellness & spa</h3>
-<p className={Styles.P}>Accommodation</p>
-<div className={Styles.left}>
-  <img className={Styles.image} src="/src/assets/arrow-right.png"/>
-</div>
-</div>
-
-
-</div>
-
-<div className={Styles.stays}>
-{
-categories.map((items)=> <div key={items} className={Styles.stay}>
-<img className={Styles.wellness2} src={items.image}/>
-<div className={Styles.inner2}>
-<h3 className={Styles.H32}>{items.title}</h3>
-<p className={Styles.P2}>{items.exp}</p>
-<div className={Styles.left2}>
-<img className={Styles.image2} src={items.arrow}/>
-</div>
-</div>
-</div>)
-}
-
-
-</div>
-
-
-
-</div>
+          <div className={Styles.stays}>
+            {categories.map((items) => (
+              <div key={items} className={Styles.stay}>
+                <img className={Styles.wellness2} src={items.image} />
+                <div className={Styles.inner2}>
+                  <h3 className={Styles.H32}>{items.title}</h3>
+                  <p className={Styles.P2}>{items.exp}</p>
+                  <div className={Styles.left2}>
+                    <img className={Styles.image2} src={items.arrow} />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default PopularCategories
-
-
-
-
+export default PopularCategories;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,37 @@
-import Discover from "../components/Discover"
-import Favourites from "../components/Favourites"
-import Navbar from "../components/Navbar"
-import PopularCategories from "../components/PopularCategories"
-import styles from "./Home.module.css"
-
+import Discover from "../components/Discover";
+import Favourites from "../components/Favourites";
+import Navbar from "../components/Navbar";
+import PopularCategories from "../components/PopularCategories";
+import styles from "./Home.module.css";
 
 function Home() {
-const Sort= [
-  
-  {
-  url:"https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac33e39149f3d14ec4154f_chalet.png",
-  name:"Chalets",
-},
-  {
-  url:"https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac33fcdb24d071131ff97f_hotel.png",
-  name:"Hotels",
-},
-  {
-  url:"https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac357998863816375543e2_fav%20(1).png",
-  name:"Our favourites",
-},
-  {
-  url:"https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac341c6d1cea5736e55299_de.png",
-  name:"EN Stays",
-},
-  {
-  url:"https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac34453f6ffd7ffaf59578_aut.png",
-  name:"EN Stays",
-},
-  {
-  url:"https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac340b2aee5f464db6b2a3_it.png",
-  name:"IT Stays",
-},
-
-]
-const card = [
+  const Sort = [
+    {
+      url: "https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac33e39149f3d14ec4154f_chalet.png",
+      name: "Chalets",
+    },
+    {
+      url: "https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac33fcdb24d071131ff97f_hotel.png",
+      name: "Hotels",
+    },
+    {
+      url: "https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac357998863816375543e2_fav%20(1).png",
+      name: "Our favourites",
+    },
+    {
+      url: "https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac341c6d1cea5736e55299_de.png",
+      name: "EN Stays",
+    },
+    {
+      url: "https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac34453f6ffd7ffaf59578_aut.png",
+      name: "EN Stays",
+    },
+    {
+      url: "https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/65ac340b2aee5f464db6b2a3_it.png",
+      name: "IT Stays",
+    },
+  ];
+  const card = [
     {
       pic: "/src/assets/beautiful.jpg",
       btn1: "chalet",
@@ -69,84 +66,74 @@ const card = [
     },
   ];
 
-  const categories= [
-  {
-  image:"/src/assets/stays-two.jpg",
-  title:"Stays for two",
-  exp:"Accommodation",
-  arrow:"/src/assets/arrow-right.png",
-},
- {
-  image:"/src/assets/adults-only.jpg",
-  title:"Adults only",
-  exp:"Accommodation",
-  arrow:"/src/assets/arrow-right.png",
-},
- {
-  image:"/src/assets/kids.jpg",
-  title:"Adults only",
-  exp:"Accommodation",
-  arrow:"/src/assets/arrow-right.png",
-},
- {
-  image:"/src/assets/cycle.jpg",
-  title:"Adults only",
-  exp:"Accommodation",
-  arrow:"/src/assets/arrow-right.png",
-},
-
-]
+  const categories = [
+    {
+      image: "/src/assets/stays-two.jpg",
+      title: "Stays for two",
+      exp: "Accommodation",
+      arrow: "/src/assets/arrow-right.png",
+    },
+    {
+      image: "/src/assets/adults-only.jpg",
+      title: "Adults only",
+      exp: "Accommodation",
+      arrow: "/src/assets/arrow-right.png",
+    },
+    {
+      image: "/src/assets/kids.jpg",
+      title: "Adults only",
+      exp: "Accommodation",
+      arrow: "/src/assets/arrow-right.png",
+    },
+    {
+      image: "/src/assets/cycle.jpg",
+      title: "Adults only",
+      exp: "Accommodation",
+      arrow: "/src/assets/arrow-right.png",
+    },
+  ];
 
   return (
     <>
+      <Navbar />
+      <div className={styles.main}>
+        {/* Container start */}
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <p className={styles.p}>In & near home:</p>
+            <h1 className={styles.h1}>
+              Experience the most wonderful accommodations
+            </h1>
+            <button className={styles.button}>Discover now</button>
+          </div>
 
-        <Navbar/>
-   <div className={styles.main}>
-   {/* Container start */}
-   <div className={styles.container}>
-<div className={styles.left}>
-<p className={styles.p}>In & near home:
+          <div className={styles.right}>
+            <button className={styles.btn1}>Hygna Chalet</button>
+            <button className={styles.btn1}>Austria</button>
+          </div>
+        </div>
+        {/* CONTAINER END */}
 
-</p>
-<h1 className={styles.h1}>
-  Experience the most wonderful accommodations
-</h1>
-<button className={styles.button}>Discover now</button>
-</div>
+        {/* SOTING SECTION START */}
+        <div className={styles.Sort}>
+          <p>Sort by:</p>
+          <div className={styles.category}>
+            {Sort.map((item) => (
+              <div key={item} className={styles.atom}>
+                <img className={styles.catimg} src={item.url} />
+                <p>{item.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* SOTING SECTION END */}
 
-<div className= {styles.right}>
-<button className={styles.btn1}>Hygna Chalet</button>
-<button className={styles.btn1}>Austria</button>
-</div>
-</div>
-{/* CONTAINER END */}
-
-
-
-{/* SOTING SECTION START */}
-<div className={styles.Sort}>
-<p>Sort by:</p>
-<div className={styles.category}> 
-{
-  Sort.map((item)=><div key={item} className={styles.atom}>
-  <img className={styles.catimg} src={item.url}/>
-<p>{item.name}</p>
-</div>  )
-}
-
-</div>
-</div>
-{/* SOTING SECTION END */}
-
-
-<PopularCategories categories={categories}/>
-<Favourites cards={card}/>
-<Discover/>
-
-   </div>
+        <PopularCategories categories={categories} />
+        <Favourites cards={card} />
+        <Discover />
+      </div>
     </>
- 
-  )
+  );
 }
 
-export default Home
+export default Home;
