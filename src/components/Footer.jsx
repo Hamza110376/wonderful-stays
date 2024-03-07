9a1665e1dac20c13d98ac936ec2f13e81215ad4a
Hamza110376 <Muhammadhamzaawan110@gmail.com>
2024-03-07 16:41:58 +0500

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,9 +32,29 @@ function Footer() {
         link: "../pages/Inspiration.jsx",
       },
     ];
+
+    // const footerItem = [
+    //   {
+    //     title: "data protection",
+    //     href: "#",
+    //   },
+    //   {
+    //     title: "terms and conditions",
+    //     href: "#",
+    //   },
+    //   {
+    //     title: "imprint",
+    //     href: "#",
+    //   },
+    //   {
+    //     title: "contact",
+    //     href: "#",
+    //   },
+    // ];
   return (
     <div className={styles.main}>
       <div className={styles.first}>
+        <img src="https://assets-global.website-files.com/659b1223cbd8d0f4420ef47b/659c7d71eba501ec8d40cf32_Group%2019.png" />
         <div className={styles.items}>
           {NavItems.map((items) => (
             <a className={styles.a} key={items} href={items.link}>
@@ -49,6 +69,11 @@ function Footer() {
           <FaLinkedin />
           <FaYoutube />
         </div>
+      </div>
+      <hr />
+
+      <div className={styles.third}>
+        <p>© 2024 Wonderful Stays. All rights reserved.</p>
       </div>
     </div>
   );
